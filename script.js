973b7c2616d16c19ad91b53cf9ed9b881c177f69
Lukas Balz <lukasbalz.business@gmail.com>
2025-06-28 20:30:20 +0200

    added profile menu

---
+++ script.js
@@ -0,0 +1,5 @@
+// Toggle Overlay
+function toggleProfileMenu() {
+    const profileMenu = document.getElementById("profileMenu");
+    profileMenu.style.display = (profileMenu.style.display == "none") ? "flex" : "none";
+}
